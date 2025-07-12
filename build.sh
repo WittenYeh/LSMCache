cca75a1d33d97fd5bd17aa3f1ec7ea7582320127
WittenYeh <2021201613@ruc.edu.cn>
2025-07-12 16:10:31 +0800

    delete useless module

--- build.sh
+++ build.sh
@@ -7,15 +7,6 @@ pip install -e "python[all]"
 pip install vllm==0.8.0 -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 # build rocksdb
-apt-get install build-essential libsnappy-dev zlib1g-dev libbz2-dev libgflags-dev liblz4-dev
-cd rocksdb
-mkdir build && cd build
-cmake ..
-make -j4
-cd ..
-export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}${CPLUS_INCLUDE_PATH:+:}`pwd`/include/
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}${LD_LIBRARY_PATH:+:}`pwd`/build/
-export LIBRARY_PATH=${LIBRARY_PATH}${LIBRARY_PATH:+:}`pwd`/build/
-
-apt-get install python3-dev
-pip install python-rocksdb
+git clone https://github.com/twmht/python-rocksdb.git --recursive -b pybind11
+cd python-rocksdb
+python setup.py install
